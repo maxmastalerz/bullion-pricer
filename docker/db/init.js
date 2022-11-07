eba72e494ab85c2366b1ad562cf2966dc7ebc139
Mohamed Moselhy <look4d@gmail.com>
2022-11-07 17:22:26 -0500

    added sample mongodb call to /api/users

---
+++ docker/db/init.js
@@ -0,0 +1,4 @@
+db.users.insertOne({
+	email: "test@example.com",
+	password: "hashedPassword",
+});
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var router = express.Router();
+const { MongoClient } = require("mongodb");
 
 /* GET home page. */
 router.get("/hello", function (req, res, next) {
 	console.log("Saying hello");
 	const { name } = req.query;
 	res.send(`<h1>Hello ${name || "world"}</h1>`);
+});
+
+router.get("/users", async (req, res) => {
+	const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
+	await client.connect();
+	const db = client.db();
+	const usersCollection = db.collection("users");
+	const users = await usersCollection.find().toArray();
+
+	res.send(users);
 });
 
 module.exports = router;
