780c39ed12f59749ae37ab634519bbdcb41c62ba
Mohamed Moselhy <look4d@gmail.com>
2022-11-29 16:49:35 -0500

    hook spot api call to db

--- api/routes/api.js
+++ api/routes/api.js
@@ -2,10 +2,10 @@ var express = require("express");
 var router = express.Router();
 const { MongoClient } = require("mongodb");
 
-async function toArray(asyncIterator) { 
-    const arr=[]; 
-    for await(const i of asyncIterator) arr.push(i); 
-    return arr;
+async function toArray(asyncIterator) {
+	const arr = [];
+	for await (const i of asyncIterator) arr.push(i);
+	return arr;
 }
 
 // PP-TODO: For all the routes that use the database, see if closing the db connection is explicitly required.
@@ -17,7 +17,7 @@ router.get("/", function (req, res, next) {
 const validEmailAddress = (emailAddress) => {
 	// PP-TODO: Make sure to better validate email address and sanitize it.
 
-	if(emailAddress === "") {
+	if (emailAddress === "") {
 		return false;
 	}
 	return true;
@@ -26,11 +26,11 @@ const validEmailAddress = (emailAddress) => {
 router.post("/subscribeToNewsletter", async (req, res) => {
 	let emailAddress = req.body.emailAddress;
 
-	if(!validEmailAddress(emailAddress)) {
+	if (!validEmailAddress(emailAddress)) {
 		res.send({
-			"data": {
-				message: `Sorry, please input a valid email address.`
-			}
+			data: {
+				message: `Sorry, please input a valid email address.`,
+			},
 		});
 		return;
 	}
@@ -44,47 +44,45 @@ router.post("/subscribeToNewsletter", async (req, res) => {
 
 	try {
 		subscriptionsCollection.insertOne({ emailAddress: emailAddress });
-	} catch(err) {
+	} catch (err) {
 		res.status(500);
 		res.send({
-			"error": {
-				message: err.message
-			}
+			error: {
+				message: err.message,
+			},
 		});
 	}
 
 	console.log(`Subscribed ${emailAddress} to newsletter.`);
 
 	res.send({
-		"data": {
-			message: `Thanks for subscribing! Stay tuned for news on the best deals.`
-		}
+		data: {
+			message: `Thanks for subscribing! Stay tuned for news on the best deals.`,
+		},
 	});
 });
 
 router.get("/spotPrices", async (req, res) => {
-	// PP-TODO: Moe, can you finish this off.
+	const currency = req.query.currency || "USD";
 
-	let currency = null;
+	const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
+	await client.connect();
+	const db = client.db();
+	const spotCollection = db.collection("spot");
+	const lastSpot = await spotCollection
+		.find({}, { limit: 1, sort: { timestamp: -1 } })
+		.toArray();
 
-	if(req.query.currency === "USD") {
-		currency = "USD";
-	}
-
-	if(currency === "USD") {
+	if (currency === "USD") {
 		res.send({
-			"data": {
-				gold: null, // example : send null if you can't get data for the metal.
-				silver: 200,
-				platinum: 300
-			}
+			data: lastSpot?.[0],
 		});
 	} else {
 		res.status(400);
 		res.send({
-			"error": {
-				message: "ERROR: Please select a valid currency."
-			}
+			error: {
+				message: "ERROR: Please select a valid currency.",
+			},
 		});
 	}
 });
@@ -97,55 +95,92 @@ router.get("/products", async (req, res) => {
 	const db = client.db();
 	const productsCollection = db.collection("products");
 
-	let purityOptions = ['99999','9999','9995','999','925','less_than_or_equal_90'];
-    let governmentNotGovernmentOptions = ['government_issued','not_government_issued'];
+	let purityOptions = [
+		"99999",
+		"9999",
+		"9995",
+		"999",
+		"925",
+		"less_than_or_equal_90",
+	];
+	let governmentNotGovernmentOptions = [
+		"government_issued",
+		"not_government_issued",
+	];
 	let nPerPage = 3;
 
-	let productTypesSelected = req.query.productTypesSelected.split(',');
-	let productSpecificsSelected = req.query.productSpecificsSelected.split(',');    
-    let purityOptionsSelected = productSpecificsSelected.filter(item => purityOptions.includes(item));
-    let governmentNotGovernmentOptionsSelected = productSpecificsSelected.filter(item => governmentNotGovernmentOptions.includes(item));
-	let paymentPreferenceSelected = req.query.paymentPreferencesSelected.split(',')[0];
+	let productTypesSelected = req.query.productTypesSelected.split(",");
+	let productSpecificsSelected =
+		req.query.productSpecificsSelected.split(",");
+	let purityOptionsSelected = productSpecificsSelected.filter((item) =>
+		purityOptions.includes(item)
+	);
+	let governmentNotGovernmentOptionsSelected =
+		productSpecificsSelected.filter((item) =>
+			governmentNotGovernmentOptions.includes(item)
+		);
+	let paymentPreferenceSelected =
+		req.query.paymentPreferencesSelected.split(",")[0];
 	let bulkPricingCouldBuy = Number(req.query.bulkPricingCouldBuy);
-	let weightRange = req.query.weightRange.split(',');
+	let weightRange = req.query.weightRange.split(",");
 	let weightStart = Number(weightRange[0]);
 	let weightEnd = Number(weightRange[1]);
 	let sortBy = Number(req.query.sortBy);
-	let sortByMap = [ // 0:priceLowToHigh 1:priceHighToLow 2:dealerAsc 3:dealerDsc 4:mintAsc 5:mintDsc
-		{ ["pricing."+paymentPreferenceSelected+".price"]: 1 },
-		{ ["pricing."+paymentPreferenceSelected+".price"]: -1 },
+	let sortByMap = [
+		// 0:priceLowToHigh 1:priceHighToLow 2:dealerAsc 3:dealerDsc 4:mintAsc 5:mintDsc
+		{ ["pricing." + paymentPreferenceSelected + ".price"]: 1 },
+		{ ["pricing." + paymentPreferenceSelected + ".price"]: -1 },
 		{ dealer: 1 },
 		{ dealer: -1 },
 		{ mint: 1 },
 		{ mint: -1 },
 	];
-	let skip = (req.query.currentPage>0) ? ((req.query.currentPage-1)*nPerPage) : 0;
+	let skip =
+		req.query.currentPage > 0 ? (req.query.currentPage - 1) * nPerPage : 0;
 
-	console.log("productTypesSelected:"+JSON.stringify(productTypesSelected));
-	console.log("productSpecificsSelected: "+JSON.stringify(productSpecificsSelected));
-	console.log("paymentPreferenceSelected: "+JSON.stringify(paymentPreferenceSelected));
-	console.log("bulkPricingCouldBuy: "+bulkPricingCouldBuy);
-	console.log("weightStart: "+weightStart);
-	console.log("weightEnd: "+weightEnd);
-	console.log("sortBy: "+sortBy);
-	console.log("skip: "+skip);
+	console.log("productTypesSelected:" + JSON.stringify(productTypesSelected));
+	console.log(
+		"productSpecificsSelected: " + JSON.stringify(productSpecificsSelected)
+	);
+	console.log(
+		"paymentPreferenceSelected: " +
+			JSON.stringify(paymentPreferenceSelected)
+	);
+	console.log("bulkPricingCouldBuy: " + bulkPricingCouldBuy);
+	console.log("weightStart: " + weightStart);
+	console.log("weightEnd: " + weightEnd);
+	console.log("sortBy: " + sortBy);
+	console.log("skip: " + skip);
 
 	const pipeline = [
 		{
 			$match: {
-				productType: {$in: productTypesSelected},
-				$and: [{
-					productSpecifics: {$in: purityOptionsSelected}}, {productSpecifics: {$in: governmentNotGovernmentOptionsSelected}
-				}],
-				weight: {$gte: weightStart, $lte: weightEnd}
-			}
+				productType: { $in: productTypesSelected },
+				$and: [
+					{
+						productSpecifics: { $in: purityOptionsSelected },
+					},
+					{
+						productSpecifics: {
+							$in: governmentNotGovernmentOptionsSelected,
+						},
+					},
+				],
+				weight: { $gte: weightStart, $lte: weightEnd },
+			},
 		},
 		{ $unwind: "$pricing" },
-		{ $unwind: "$pricing."+paymentPreferenceSelected },
-		{ $match : {
-			["pricing."+paymentPreferenceSelected+".qtyRange.0"]: { $lte: bulkPricingCouldBuy },
-			["pricing."+paymentPreferenceSelected+".qtyRange.1"]: { $gte: bulkPricingCouldBuy }
-		}},
+		{ $unwind: "$pricing." + paymentPreferenceSelected },
+		{
+			$match: {
+				["pricing." + paymentPreferenceSelected + ".qtyRange.0"]: {
+					$lte: bulkPricingCouldBuy,
+				},
+				["pricing." + paymentPreferenceSelected + ".qtyRange.1"]: {
+					$gte: bulkPricingCouldBuy,
+				},
+			},
+		},
 		{
 			$project: {
 				url: 1,
@@ -155,19 +190,16 @@ router.get("/products", async (req, res) => {
 				productSpecifics: 1,
 				productType: 1,
 				weight: 1,
-				["pricing."+paymentPreferenceSelected]: 1
-			}
+				["pricing." + paymentPreferenceSelected]: 1,
+			},
 		},
 		{ $sort: sortByMap[sortBy] },
-		{ "$facet": {
-			"totalData": [
-				{ $skip: skip },
-				{ $limit: nPerPage }
-			],
-			"totalCount": [
-				{ "$count": "count" }
-			]
-		}}
+		{
+			$facet: {
+				totalData: [{ $skip: skip }, { $limit: nPerPage }],
+				totalCount: [{ $count: "count" }],
+			},
+		},
 	];
 
 	const products = await toArray(productsCollection.aggregate(pipeline));
