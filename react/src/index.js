b63d0a3ec272d0bcf7b1697b0906d1f9358fc131
Max Mastalerz <contact@maxmastalerz.com>
2024-01-09 09:16:35 -0500

    removed some packages and change npm pull repository

--- react/src/index.js
+++ react/src/index.js
@@ -9,9 +9,6 @@ import { store } from './store/store';
 // Css
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-slider/dist/css/bootstrap-slider.css"
-import '../node_modules/slick-carousel/slick/slick.css';
-import '../node_modules/slick-carousel/slick/slick-theme.css';
-import '../node_modules/animate.css/animate.css';
 import './assets/css/font-awesome.min.css';
 import './assets/css/flaticon.css';
 import './assets/fonts/flaticon/flaticon-2.css';
